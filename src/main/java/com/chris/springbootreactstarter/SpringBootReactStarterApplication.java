a5d4c1c67f95564caa58c192b3a585e9c81d777b
Chris Beauclair <chris.beauclair@gmail.com>
2018-01-12 14:42:24 -0500

    Initiall Commit

---
+++ src/main/java/com/chris/springbootreactstarter/SpringBootReactStarterApplication.java
@@ -0,0 +1,12 @@
+package com.chris.springbootreactstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactStarterApplication.class, args);
+	}
+}
---
+++ src/test/java/com/chris/springbootreactstarter/SpringBootReactStarterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chris.springbootreactstarter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootReactStarterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
